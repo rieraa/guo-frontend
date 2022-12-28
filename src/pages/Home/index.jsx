3dd7416346954056f81b7b3431b1de9575741ac0
HCX <1874640833@qq.com>
2022-12-28 17:33:07 +0800

    增加跳转到章节页

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import cover from '../../assets/coverIMG.jpg';
 import Pagination_Ti from '../../components/Pagination';
 import { http } from '../../utils/http';
 const StudyHome = () => {
+  const navigate = useNavigate();
   const [courese, setCourse] = useState([]);
   useEffect(() => {
     const getList = async () => {
@@ -22,7 +24,10 @@ const StudyHome = () => {
           return (
             <div
               className=' mx-3 mb-6 h-64 w-64 overflow-hidden bg-white hover:shadow-lg hover:cursor-pointer rounded-lg'
-              key={item.courseId}>
+              key={item.courseId}
+              onClick={() => {
+                navigate(`chapter?id=${item.courseId}`);
+              }}>
               <div className=' w-full h-44'>
                 <img className='w-full h-full' src={cover} alt='' />
               </div>
