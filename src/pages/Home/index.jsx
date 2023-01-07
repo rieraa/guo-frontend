a26b767d3f26b47cc360e45f049770e1ab4cd452
HCX <1874640833@qq.com>
2023-01-07 22:44:57 +0800

    轮播图实现

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Swiper, Input } from 'tdesign-react';
 import cover from '../../assets/coverIMG.jpg';
 import Pagination_Ti from '../../components/Pagination';
 import { http } from '../../utils/http';
+const { SwiperItem } = Swiper;
 const StudyHome = () => {
   const navigate = useNavigate();
   const [courese, setCourse] = useState([]);
+  const [value, onChange] = useState('Hello TDesign');
   useEffect(() => {
     const getList = async () => {
       const res = await http.post('/api/courseinfo/search', {});
@@ -16,9 +19,61 @@ const StudyHome = () => {
   }, []);
 
   return (
-    <div className=' mx-auto px-12 bg-slate-100 '>
+    <div className=' mx-auto px-14 bg-slate-100'>
+      <div className='flex p-4'>
+        <div style={{ width: '600px', height: '300px' }}>
+          <Swiper duration={300} interval={2000} autoplay trigger='hover'>
+            <SwiperItem>
+              <div
+                style={{ width: '600px', height: '300px' }}
+                className='demo-item'>
+                <img className='h-full w-full' src={cover} alt='' />
+              </div>
+            </SwiperItem>
+            <SwiperItem>
+              <div
+                style={{ width: '600px', height: '300px' }}
+                className='demo-item'>
+                <img className='h-full w-full' src={cover} alt='' />
+              </div>
+            </SwiperItem>
+            <SwiperItem>
+              <div
+                style={{ width: '600px', height: '300px' }}
+                className='demo-item'>
+                <img className='h-full w-full' src={cover} alt='' />
+              </div>
+            </SwiperItem>
+          </Swiper>
+        </div>
+        <div className=' pl-7 text-slate-700'>
+          <h1 className=' text-2xl text-center font-semibold pb-4'>
+            慕课学习网站
+          </h1>
+          <p className='text-lg'>
+            我们的慕课学习网站是一个专门提供在线教育课程的网站。我们拥有大量优质的课程，包括计算机科学、人工智能、数据科学、编程、设计等领域的课程。我们的课程由经验丰富的讲师授课，并配有丰富的视频讲解和练习题，方便学习。
+            我们的课程都是由经验丰富的讲师讲授，并配有丰富的视频讲解和练习题。
+            课程内容系统全面，能够帮助学生从零基础入门，也能够帮助有经验的学习者提升技能。我们的课程具有较高的实用价值，旨在帮助学生掌握实用技能，为他们的职业生涯做好准备。
+            我们希望能够为您的学习提供帮助，让您在我们的慕课学习网站上有收获、有成就感。
+          </p>
+        </div>
+      </div>
+      <div>
+        <Input
+          placeholder='请输入内容'
+          value={value}
+          clearable
+          onChange={(value) => {
+            onChange(value);
+          }}
+          onClear={() => {
+            console.log('onClear');
+          }}
+        />
+      </div>
+
       {/* list start*/}
-      <div className=' flex flex-wrap pt-6 justify-start '>
+      <div className=' flex flex-wrap pt-12 justify-start '>
         {/* card start*/}
         {courese.map((item) => {
           return (
